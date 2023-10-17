632d6068c4b5c81cc43683c72a733e2e33472f14
BuiDinhVan <dinhvanvtpt123@gmail.com>
2023-10-17 19:54:35 +0700

    van upload code payment.momo

--- routers/payment.js
+++ routers/payment.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { PaymentVNPay } from "../controllers/payment.js";
+import { PaymentMOMO } from "../controllers/payment.momo.js";
 const router = express.Router();
-router.post("/create_payment_url", PaymentVNPay);
+router.post("/create_paymentVnpay_url", PaymentVNPay);
+router.post("/create_paymentMoMo_url", PaymentMOMO);
 
 // router.get("/https://google.com/");
 // router.get("/", getCategories);
